17580317528a5891ec8e45f31d2694c15a689b14
Angela Wirth <angiewirth92@gmail.com>
2023-02-21 13:11:52 -0500

    Remove original placeholders in h2 and h3s. Update styling to match spec

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -7,7 +7,7 @@ const Card = ({name, date, time, guests}) => {
       <h2 className='font-large'>{name}</h2>
       <h3 className='font-med'>{date}</h3>
       <h3 className='font-med'>{time}</h3>
-      <h3 className='font-med'>Number of guests:{guests}</h3>
+      <h3 className='font-med'>Number of guests: {guests}</h3>
       <button className='cancel-button'>Cancel</button>
     </div>
   )
