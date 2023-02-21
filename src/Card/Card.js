b5fee1b52d67322786dab1e7a05b4d8346b398ff
Angela Wirth <angiewirth92@gmail.com>
2023-02-21 12:44:09 -0500

    Update card tags to equal the props passed down

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Card.css'
 
-const Card = ({}) => {
+const Card = ({name, date, time, guests}) => {
   return (
     <div className='card-container'>  
-      <h2 className='font-large'>Name:</h2>
-      <h3 className='font-med'>Date:</h3>
-      <h3 className='font-med'>Time:</h3>
-      <h3 className='font-med'>Number of guests:</h3>
+      <h2 className='font-large'>{name}</h2>
+      <h3 className='font-med'>{date}</h3>
+      <h3 className='font-med'>{time}</h3>
+      <h3 className='font-med'>Number of guests:{guests}</h3>
       <button className='cancel-button'>Cancel</button>
     </div>
   )
