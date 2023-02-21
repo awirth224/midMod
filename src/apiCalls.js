1f8d14d6ac948bf76e292d3f964666777f11172a
Angela Wirth <angiewirth92@gmail.com>
2023-02-21 11:43:55 -0500

    Setup GET request for reservations

---
+++ src/apiCalls.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const getReservations = () => {
+  return fetch('http://localhost:3001/api/v1/reservations')
+    .then(response => response.json())
+}
+
+export default getReservations
