1beb498d6daef49f65e562769c924b0e73330b06
Angela Wirth <angiewirth92@gmail.com>
2023-02-21 14:06:53 -0500

    Write tests for visiting the page, checking for a title, and checking for an input

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,34 @@
+beforeEach(() => {
+  cy.intercept('http://localhost:3001/api/v1/reservations', [
+    {
+      id: 1,
+      name: "Christie",
+      date: "12/29",
+      time: "7:00",
+      number: 12
+    },
+    {
+      id: 2,
+      name: "Leta",
+      date: "4/5",
+      time: "7:00",
+      number: 2
+      },
+      {
+      id: 3,
+      name: "Pam",
+      date: "1/21",
+      time: "6:00",
+      number: 4
+      }
+  ]
+)
+})
+
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('should be able to visit the page and render the correct elements', () => {
+    cy.visit('http://localhost:3000/')
+    .contains('h1', 'Turing Cafe Reservations')
+    .get('form').find('input')
   })
 })
